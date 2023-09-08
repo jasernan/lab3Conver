e9aea855d195f50ed4d9f610cda8794d96e17964
jasernan <julian-serna@upc.edu.co>
2023-09-08 18:22:35 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/unipiloto/lab3/pag43.java
@@ -0,0 +1,14 @@
+package co.edu.unipiloto.lab3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class pag43 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pag43);
+
+    }
+}
